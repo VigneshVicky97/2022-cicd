ce55bae2cf49fe9cf71b615e69830b8234420590
vignesh <vigneshs@melss.com>
2023-12-26 14:54:41 +0530

    Added Comments

--- tests/test_channel_ops.py
+++ tests/test_channel_ops.py
@@ -2,18 +2,21 @@ from functools import partial
 
 import pytest
 
-# from ..functions.operations import ChannelNotFoundError, get_channel
+# from ..functions.operations import ChannelNotFoundError, get_channel  # This line causes import error during pytest
 from functions.operations import ChannelNotFoundError, get_channel
 
+# Location of Mock Channles Database
 MOCK_DB = "tests/mock_channels.db"
 get_channel_mock = partial(get_channel, db_path=MOCK_DB)
 
 
+# Test Channel Success
 def test_get_channel_success() -> None:
     channel = get_channel_mock("arjancodes")
     assert channel["name"] == "ArjanCodes"
 
 
+#Test Channel Failure
 def test_get_channel_fail() -> None:
     with pytest.raises(ChannelNotFoundError):
         get_channel_mock("arjancodes123")
